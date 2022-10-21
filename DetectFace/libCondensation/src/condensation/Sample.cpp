9f021b7227512c319a1f6612a257e50a6590ef83
Gerald <geraldwainaina25@gmail.com>
2022-10-21 14:57:05 +0300

    Create Sample.cpp

---
+++ DetectFace/libCondensation/src/condensation/Sample.cpp
@@ -0,0 +1,15 @@
+/*
+ * Sample.cpp
+ *
+ *  Created on: 11.06.2013
+ *      Author: poschmann
+ */
+
+#include "condensation/Sample.hpp"
+
+namespace condensation {
+
+double Sample::aspectRatio = 1;
+int Sample::nextClusterId = 0;
+
+} /* namespace condensation */
